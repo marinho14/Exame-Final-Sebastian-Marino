495dd5e7142318b6150b41896b2a7eaa6755648f
marinho14 <marinosebastian5@gmail.com>
2021-11-24 18:49:54 -0500

    Commit final, con fe

--- punto3.py
+++ punto3.py
@@ -3,6 +3,12 @@ import numpy as np
 
 
 def punto_3():
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale = 1
+    color = (255, 0, 0)
+    thickness = 2
+
     ## Se define la funcion de click
     def click(event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
@@ -19,6 +25,7 @@ def punto_3():
     cv2.namedWindow("Image")
     cv2.setMouseCallback("Image", click)
     W= image.shape[1]
+    p=0
 
     ## Se define donde se debe graficar la linea
     point_counter = 0
@@ -26,12 +33,15 @@ def punto_3():
         cv2.imshow("Image", image_draw)
         key = cv2.waitKey(1) & 0xFF
         if len(points)==3:
+            cv2.putText(image_draw, 'p' + str(p), (points[-1][0], points[-1][1]), font, fontScale, color, thickness,cv2.LINE_AA)
             points1 = points.copy()
             points = []
             break
         if len(points) > point_counter:
             point_counter = len(points)
             cv2.circle(image_draw, (points[-1][0], points[-1][1]), 3, [0, 0, 255], -1)
+            cv2.putText(image_draw, 'p'+str(p), (points[-1][0], points[-1][1]), font,fontScale, color, thickness, cv2.LINE_AA)
+            p=p+1
 
 
     ## Se encuentran los puntos X y Y de los puntos escogidos
@@ -66,10 +76,10 @@ def punto_3():
     Y_ini_2= int(X_ini*pendiente+b2)
     Y_final_2=int(X_final*pendiente+b2)
 
-    magenta = (255, 0, 255)   ## Se define el color magenta
-    rojo = (0, 0, 0)
-    image = cv2.line(image_draw, (X_ini,Y_ini), (X_final,Y_final), magenta, 2)
-    image = cv2.line(image_draw, (X_ini,Y_ini_2), (X_final,Y_final_2), rojo, 2)
+    azul = (255, 0, 0)   ## Se define el color magenta
+    amarillo = (0, 255, 255)
+    image = cv2.line(image_draw, (X_ini,Y_ini), (X_final,Y_final), azul, 2)
+    image = cv2.line(image_draw, (X_ini,Y_ini_2), (X_final,Y_final_2), amarillo, 2)
 
     cv2.imshow("Image2", image)
     cv2.waitKey(0)
